44f480e980eb098509f12652b2030d09c2f3e88f
tiago.cipriano <tiago.cipriano@luilzalabs.com>
2025-05-07 22:47:48 -0300

    chore: adiciona casos do zero

--- test/validaCpf2.test.ts
+++ test/validaCpf2.test.ts
@@ -11,3 +11,26 @@ test("Deve testar um cpf válido", async () => {
   expect(isValid).toBe(true);
 });
 
+
+test("Deve testar um cpf válido com final zero", async () => {
+  // Given
+  const cpf = "71428793860";
+
+  // When
+  const isValid = validateCpf2(cpf);
+
+  // Then
+  expect(isValid).toBe(true);
+});
+
+
+test("Deve testar um cpf válido com final dois zeros", async () => {
+  // Given
+  const cpf = "87748248800";
+
+  // When
+  const isValid = validateCpf2(cpf);
+
+  // Then
+  expect(isValid).toBe(true);
+});
