4ba7605858f9bd0b048ad445f14d1d16fde4e5ae
tiago.cipriano <tiago.cipriano@luilzalabs.com>
2025-05-09 23:58:16 -0300

    refactor: fix local and type of declarations

--- src/validateCpf2.ts
+++ src/validateCpf2.ts
@@ -2,13 +2,12 @@ const CLEAR_CPF_REGEX = /[.,\-\s]/g;
 const CPF_LEN = 11;
 
 export function validateCpf2(str: string): boolean {
-  if (str === null) return false;
-  if (str === undefined) return false;
+  if (isInvalid(str)) return false;
   str = sanitize(str, CLEAR_CPF_REGEX);
   if (str.length !== CPF_LEN) return false;
   if (str.split("").every(c=> c===str[0])) return false;
   let d1 = 0, d2 = 0;
-  for (let nCount = 1; nCount < str.length -1; nCount ++){
+  for (let nCount = 1; nCount < CPF_LEN -1; nCount ++){
     const digit = parseInt(str.substring(nCount-1, nCount))
     d1 = d1 + (CPF_LEN - nCount) * digit;
     d2 = d2 + (12 - nCount) * digit;
@@ -17,13 +16,9 @@ export function validateCpf2(str: string): boolean {
   const dg1 = (rest < 2) ? 0 : CPF_LEN - rest;
   d2 += 2 * dg1
   rest = (d2 % CPF_LEN);
-  let dg2 = CPF_LEN -  rest;
-  if (rest < 2 )
-    dg2 = 0;
-  let  nDigVerific = str.substring(str.length-2, str.length)
+  const dg2 = (rest < 2 ) ? 0 : CPF_LEN -  rest;
+  let  nDigVerific = str.substring(CPF_LEN-2, CPF_LEN)
   return nDigVerific == `${dg1}${dg2}`;
 }
-
-const sanitize = (s: string, pattern: RegExp):string => {
-  return s.replace(pattern,'');
-}
+const sanitize = (s: string, pattern: RegExp):string => s.replace(pattern,'');
+const isInvalid = (i: any): boolean => i === null || i === undefined;
