061edb30d1ab0a9cd8c6edef9254bfb6d5e9d7d6
tiago.cipriano <tiago.cipriano@luilzalabs.com>
2025-05-09 23:16:28 -0300

    chore: define const near use

--- src/validateCpf2.ts
+++ src/validateCpf2.ts
@@ -8,13 +8,13 @@ export function validateCpf2(str: string): boolean {
   // tudo igual
   if (str.split("").every(c=> c===str[0])) return false;
   try {
-    let d1, d2, dg1, dg2, digito, nDigResult;
+    let d1, d2, dg1, dg2;
     d1 = d2 = 0;
     dg1 = dg2 = 0;
     for (let nCount = 1; nCount < str.length -1; nCount ++){
-      digito = parseInt(str.substring(nCount-1, nCount))
-      d1 = d1 + (11 - nCount) * digito;
-      d2 = d2 + (12 - nCount) * digito;
+      const digit = parseInt(str.substring(nCount-1, nCount))
+      d1 = d1 + (11 - nCount) * digit;
+      d2 = d2 + (12 - nCount) * digit;
     }
     let rest = (d1 % 11);
     dg1 = (rest < 2) ? 0 : 11 - rest;
@@ -25,7 +25,7 @@ export function validateCpf2(str: string): boolean {
     else
       dg2 = 11 -  rest;
     let  nDigVerific = str.substring(str.length-2, str.length)
-    nDigResult = ""+dg1+""+dg2;
+    const nDigResult = ""+dg1+""+dg2;
     return nDigVerific == nDigResult
   }catch(e){
     console.log('Erro: ' + e)
