bd65b94648c7db1e2220cf7773e5338bf58821f2
tiago.cipriano <tiago.cipriano@luilzalabs.com>
2025-05-13 15:49:51 -0300

    chore: create cpf validate on create account
    chore: add password field to account
    
    test: chage default axios behave to return response on diff 200 or 201
    
    test: create basic validate to create account

--- src/signup3.ts
+++ src/signup3.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from "express";
 import crypto from "crypto";
+import {validateCpf} from "./validateCpf";
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,7 @@ interface Account {
   name: string
   email: string
   document: string
+  password: string
 }
 
 interface AccountDb<Account> {
@@ -17,14 +19,37 @@ interface AccountDb<Account> {
 
 const dbMem: AccountDb<Account> = {};
 
+function invalidPassword (password: string) {
+    if (password.length < 8) return true;
+    if (!password.match(/\d+/)) return true;
+    if (!password.match(/[a-z]+/)) return true;
+    if (!password.match(/[A-Z]+/)) return true;
+    return false;
+}
+const invalidName = (name: string):boolean => {
+  const regex = /^\w+\s+\w+.*$/;
+  return !regex.test(name);
+}
+
+const invalidEmail = (email: string):boolean => {
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return !regex.test(email);
+}
+
 app.post('/signup', async (req: Request, res: Response) => {
   const accountId = crypto.randomUUID();
   const input = req.body;
+  if (invalidName(input.name)) return res.status(422).json({"error": "Invalid Name"})
+  if (invalidEmail(input.email)) return res.status(422).json({"error": "Invalid Email"})
+  if (!validateCpf(input.document)) return res.status(422).json({"error": "Invalid Document"})
+  if (invalidPassword(input.password)) return res.status(422).json({"error": "Invalid Password"})
+
   dbMem[accountId] = {
     accountId: accountId,
     name: input.name,
     email: input.email,
     document: input.document,
+    password: input.password,
   }
   res.status(201).json({"accountId": accountId})
   console.log('oi')
--- test/signup.2.test.ts
+++ test/signup.2.test.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const server = "http://localhost:3000";
+axios.defaults.validateStatus = () => true;
 
 test('deve criar uma conta', async () => {
   // given
@@ -26,3 +27,66 @@ test('deve criar uma conta', async () => {
   // TODO - crie uma forma de não retornar email e crie a expectativa para essa propiedade
   // não deve existir a propriedade password no get account
 });
+
+test('não deve criar uma conta com nome inválido', async () => {
+  // given
+  const inputSignup = {
+    "name": "Tester",
+    "email": "tester@email.com",
+    "document": "97456321558",
+    "password": "Paww0rdsw"
+  }
+
+  // then
+  const resSignup = await axios.post(`${server}/signup`, inputSignup);
+  expect(resSignup.status).toBe(422);
+  expect(resSignup.data.error).toBe("Invalid Name");
+});
+
+
+test('não deve criar uma conta com email inválido', async () => {
+  // given
+  const inputSignup = {
+    "name": "Tester Useer",
+    "email": "testeremail.com",
+    "document": "97456321558",
+    "password": "Paww0rdsw"
+  }
+
+  // then
+  const resSignup = await axios.post(`${server}/signup`, inputSignup);
+  expect(resSignup.status).toBe(422);
+  expect(resSignup.data.error).toBe("Invalid Email");
+});
+
+test('não deve criar uma conta com cpf inválido', async () => {
+  // given
+  const inputSignup = {
+    "name": "Tester Useer",
+    "email": "tester@email.com",
+    "document": "9746321558",
+    "password": "Paww0rdsw"
+  }
+
+  // then
+  const resSignup = await axios.post(`${server}/signup`, inputSignup);
+  expect(resSignup.status).toBe(422);
+  expect(resSignup.data.error).toBe("Invalid Document");
+});
+
+test('não deve criar uma conta com senha inválida', async () => {
+  // given
+  const inputSignup = {
+    "name": "Tester Useer",
+    "email": "tester@email.com",
+    "document": "97456321558",
+    "password": "Pawwrdsw"
+  }
+
+  // then
+  const resSignup = await axios.post(`${server}/signup`, inputSignup);
+  expect(resSignup.status).toBe(422);
+  expect(resSignup.data.error).toBe("Invalid Password");
+});
+
+
