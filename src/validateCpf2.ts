63f3f8cbae8fecf9f6d344c0d1437630c7e2e792
TiagoTi <tiago@ti2.net.br>
2025-05-14 15:06:29 -0300

    chore: extrac code to func

--- src/validateCpf2.ts
+++ src/validateCpf2.ts
@@ -1,14 +1,21 @@
 const CLEAR_CPF_REGEX = /[.,\-\s]/g;
 const CPF_LEN = 11;
 
-export function validateCpf2(str: string): boolean {
-  if (isInvalid(str)) return false;
-  str = sanitize(str, CLEAR_CPF_REGEX);
-  if (str.length !== CPF_LEN) return false;
-  if (str.split("").every(c=> c===str[0])) return false;
+export function validateCpf2(cpf: string): boolean {
+  if (isInvalid(cpf)) return false;
+  cpf = sanitize(cpf, CLEAR_CPF_REGEX);
+  if (invalidLength(cpf)) return false;
+  if (sameChar(cpf)) return false;
+  return validate(cpf);
+}
+const isInvalid = (i: any): boolean => i === null || i === undefined;
+const sanitize = (s: string, pattern: RegExp):string => s.replace(pattern,'');
+const invalidLength = (s: string): boolean => s.length !== CPF_LEN;
+const sameChar = (s: string): boolean => s.split("").every(c=> c===s[0]);
+const validate = (cpf: string): boolean => {
   let d1 = 0, d2 = 0;
   for (let nCount = 1; nCount < CPF_LEN -1; nCount ++){
-    const digit = parseInt(str.substring(nCount-1, nCount))
+    const digit = parseInt(cpf.substring(nCount-1, nCount))
     d1 = d1 + (CPF_LEN - nCount) * digit;
     d2 = d2 + (12 - nCount) * digit;
   }
@@ -17,8 +24,6 @@ export function validateCpf2(str: string): boolean {
   d2 += 2 * dg1
   rest = (d2 % CPF_LEN);
   const dg2 = (rest < 2 ) ? 0 : CPF_LEN -  rest;
-  let  nDigVerific = str.substring(CPF_LEN-2, CPF_LEN)
+  let  nDigVerific = cpf.substring(CPF_LEN-2, CPF_LEN)
   return nDigVerific == `${dg1}${dg2}`;
 }
-const sanitize = (s: string, pattern: RegExp):string => s.replace(pattern,'');
-const isInvalid = (i: any): boolean => i === null || i === undefined;
